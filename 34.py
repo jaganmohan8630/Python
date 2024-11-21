a8e68efd29683f289549f73dfb5e0479394ad98f
Jagan Mohan Jangam <jaganmohanjangam9@gmail.com>
2024-11-21 07:51:50 +0530

    Jagan Mohan

---
+++ 34.py
@@ -0,0 +1,4 @@
+def volume(a):
+    return a**3
+a=int(input("Enter a:"))
+print("Volume of cube is ",volume(a))
---
+++ 35.py
@@ -0,0 +1,6 @@
+#  write a function to convert time into minutes 
+# def convert(hours,minutes): # function header 
+# convert( 7, 30) # function call statement
+def convert(hours,minutes):
+    return 60*hours + minutes
+print(convert(7,30))
